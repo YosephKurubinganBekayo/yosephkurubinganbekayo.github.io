2fb82940e5196ccbf1a1012352ea9cab5a8cdef3
YosephKurubinganBekayo <102840619+YosephKurubinganBekayo@users.noreply.github.com>
2024-12-20 21:34:01 +0800

    Update koneksi.php

--- admin/inc/koneksi.php
+++ admin/inc/koneksi.php
@@ -1,2 +1,2 @@
 <?php
-$koneksi = new mysqli("localhost", "u881769443_dbarspus	", "*IH=?wmkFy1", "u881769443_dbarspus");
+$koneksi = new mysqli("localhost", "u881769443_dbarspus", "*IH=?wmkFy1", "u881769443_dbarspus");
